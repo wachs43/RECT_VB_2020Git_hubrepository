9fab6eafd75d862b18f2fd82a6da8d6727cd9773
Alex Wachs <alexwachs310@hotmail.com>
2020-01-22 08:40:42 -0700

    add project

---
+++ SayMyName/Module1.vb
@@ -0,0 +1,21 @@
+﻿'Alex Wachs
+'RCET0265
+'Spring 2020
+'Say My Name
+'RECT_VB_2020Git_hubrepository
+
+
+Module Module1
+
+    Sub Main()
+        Dim yourname As String
+        Console.WriteLine("gooooood morning vietnam!!!!!!")
+        Console.WriteLine("who do we have with us today")
+        yourname = Console.ReadLine()
+        Console.WriteLine("nice to meet you " & yourname & " lets just be friends")
+        Console.ReadLine()
+
+
+    End Sub
+
+End Module
