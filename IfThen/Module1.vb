37ecc3995b9e1d1165124484d17ff92d7a31d554
Alex Wachs <alexwachs310@hotmail.com>
2020-01-23 08:40:29 -0700

    finished up

--- IfThen/Module1.vb
+++ IfThen/Module1.vb
@@ -1,12 +1,27 @@
 ﻿Module Module1
 
     Sub Main()
-        If 1 > 2 Then
-            Console.WriteLine("True")
+        Dim firstnumber As Integer
+        Dim secoundnumber As Integer
+
+        firstnumber = 3
+        secoundnumber = 5
+
+        If firstnumber > secoundnumber Then
+            Console.WriteLine("first number is larger")
+        ElseIf firstnumber < secoundnumber Then
+            Console.WriteLine("secound number is larger")
+        ElseIf firstnumber >= secoundnumber Then
+            Console.WriteLine("first number is bigger or equal to")
+        ElseIf firstnumber <= secoundnumber Then
+            Console.WriteLine("the first number is smaller or equal")
+        ElseIf firstnumber <> secoundnumber Then
+            Console.WriteLine("the numbers are not equal")
         Else
-            Console.WriteLine("False")
+            Console.WriteLine("something other")
 
         End If
+        Console.ReadLine()
     End Sub
 
 End Module
