f7450f5c66bf0ba33d56d2dd6cca9fc6034c299f
Alex Wachs <alexwachs310@hotmail.com>
2020-01-23 08:23:14 -0700

    if then codeing part 1

---
+++ IfThen/Module1.vb
@@ -0,0 +1,12 @@
+﻿Module Module1
+
+    Sub Main()
+        If 1 > 2 Then
+            Console.WriteLine("True")
+        Else
+            Console.WriteLine("False")
+
+        End If
+    End Sub
+
+End Module
