f79f92998692e6d1793741ba97ec9aa6df1dd03d
Alex Wachs <alexwachs310@hotmail.com>
2020-01-24 08:55:52 -0700

    in class calc

---
+++ simple calculator/Module1.vb
@@ -0,0 +1,17 @@
+﻿
+
+Module Module1
+
+    Sub Main()
+        Dim firstnumber As Int32
+        Dim secoundnumber As Int32
+        firstnumber = Console.ReadLine()
+        secoundnumber = Console.ReadLine()
+
+        Console.WriteLine(firstnumber + secoundnumber)
+        Console.ReadLine()
+        'cint(variable) converts a sting to variable
+
+    End Sub
+
+End Module
