7f0d90d2a861b1ecb85d3987e7f120a020760f16
Alex Wachs <alexwachs310@hotmail.com>
2020-01-27 19:49:39 -0700

    day 2

--- simple calculator/Module1.vb
+++ simple calculator/Module1.vb
@@ -10,12 +10,28 @@ Module Module1
     Sub Main()
         Dim firstnumber As Int32
         Dim secoundnumber As Int32
+        Dim selector As Int32
+
+        Console.WriteLine("enter 1st digit, enter secound digit, select 1 for addition and 2 for multiplication.")
         firstnumber = Console.ReadLine()
         secoundnumber = Console.ReadLine()
+        selector = Console.ReadLine()
 
-        Console.WriteLine(firstnumber + secoundnumber)
+
+
+        If selector = 1 Then
+            Console.WriteLine(firstnumber + secoundnumber)
+            Console.ReadLine()
+            'cint(variable) converts a sting to variable
+        ElseIf selector = 2 Then
+            Console.WriteLine(firstnumber * secoundnumber)
+            Console.ReadLine()
+        Else
+            Console.WriteLine("invalid operation")
+            Console.ReadLine()
+        End If
+        Console.WriteLine("press enter to end")
         Console.ReadLine()
-        'cint(variable) converts a sting to variable
 
     End Sub
 
