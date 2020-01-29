f962c33a18dcd26d126c15c12f727a7505cd4265
Alex Wachs <alexwachs310@hotmail.com>
2020-01-29 08:58:33 -0700

    oasmvo=n

--- simple calculator/Module1.vb
+++ simple calculator/Module1.vb
@@ -4,6 +4,9 @@
 'SIMPLE CALC
 'RCET_VB_2020
 
+Option Explicit On
+Option Compare Binary
+
 
 Module Module1
 
@@ -11,28 +14,64 @@ Module Module1
         Dim firstnumber As Int32
         Dim secoundnumber As Int32
         Dim selector As Int32
+        Dim QUIT As Boolean
+        Dim userpromt As Boolean
 
-        Console.WriteLine("enter 1st digit, enter secound digit, select 1 for addition and 2 for multiplication.")
-        firstnumber = Console.ReadLine()
-        secoundnumber = Console.ReadLine()
-        selector = Console.ReadLine()
+        userpromt = True
+        Do While QUIT = False
+
+            Console.WriteLine("enter 1st digit, enter secound digit, select 1 for addition and 2 for multiplication.")
+
+            Console.WriteLine("please enter a number")
+            Do While userpromt = True
+                Try
+                    firstnumber = CInt(Console.ReadLine())
+                    userpromt = False
+                Catch ex As Exception
+                    Console.WriteLine("please enter a whole number")
+                    userpromt = True
+                End Try
+            Loop
+            Do While userpromt = False
+
+
+                Try
+                    secoundnumber = CInt(Console.ReadLine())
+                    userpromt = True
+                Catch ex As Exception
+                    Console.WriteLine("please enter a whole number")
+                    userpromt = False
+
+                End Try
+            Loop
 
 
 
-        If selector = 1 Then
-            Console.WriteLine(firstnumber + secoundnumber)
-            Console.ReadLine()
-            'cint(variable) converts a sting to variable
-        ElseIf selector = 2 Then
-            Console.WriteLine(firstnumber * secoundnumber)
-            Console.ReadLine()
-        Else
-            Console.WriteLine("invalid operation")
-            Console.ReadLine()
-        End If
-        Console.WriteLine("press enter to end")
-        Console.ReadLine()
+            selector = Console.ReadLine()
 
+
+
+                If selector = 1 Then
+                    Console.WriteLine(firstnumber + secoundnumber)
+                    Console.ReadLine()
+                    'cint(variable) converts a sting to variable
+                ElseIf selector = 2 Then
+                    Console.WriteLine(firstnumber * secoundnumber)
+                    Console.ReadLine()
+                Else
+                    Console.WriteLine("invalid operation")
+                    Console.ReadLine()
+                End If
+                Console.WriteLine("HAVE A NICE DAY!!!")
+                Console.WriteLine("PRESS ANYKEY To RUN AGAIN PRESS q To QUIT")
+                If Console.ReadLine() = "q" Then
+                    QUIT = True
+                Else
+                    QUIT = False
+                End If
+
+                Console.Clear()
+            Loop
     End Sub
 
 End Module
