7e06e8ebb55b12d33bf6bda8b5a6171f9c767f61
Alex Wachs <alexwachs310@hotmail.com>
2020-01-30 08:58:17 -0700

    subssss

---
+++ SubFunctionExample/Module1.vb
@@ -0,0 +1,45 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim mynumber As Integer
+        mynumber = 7
+        Console.WriteLine("in sub main")
+        Console.WriteLine("press enter to continue")
+        Console.WriteLine(mynumber)
+        Console.ReadLine()
+        mysub(mynumber)
+        Console.WriteLine("in sub main")
+        Console.WriteLine(mynumber)
+        mynumber = myfunc(mynumber)
+        Console.WriteLine("in sub main")
+        Console.WriteLine(mynumber)
+        Console.ReadLine()
+
+
+    End Sub
+
+    Sub mysub(mynumber As Integer)
+        ' Dim mynumber As Integer
+        mynumber += 2
+
+        Console.WriteLine("in my sub")
+        Console.WriteLine(mynumber)
+
+        Console.ReadLine()
+
+
+    End Sub
+    Function myfunc(mynumber As Integer) As Integer
+        'Dim mynumber As Integer
+        mynumber -= 3
+
+        Console.WriteLine("in function MyFunc")
+        Console.WriteLine(mynumber)
+        Console.ReadLine()
+
+        Return mynumber
+    End Function
+
+
+
+End Module
