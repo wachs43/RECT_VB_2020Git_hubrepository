9a1da5c14629d3b9ef8a43f9769e1c19cda32be5
Alex Wachs <alexwachs310@hotmail.com>
2020-02-12 09:00:42 -0700

    dataform

---
+++ VALIDATEUSERDATAFORM/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
+    Sub isempty()
+        Dim usermessage As String
+
+        If TextBox1.Text = "" Then
+            usermessage &= "please enter text, textbox 1" & vbNewLine
+            TextBox1.Select()
+        End If
+        If TextBox2.Text = "" Then
+            usermessage &= "please enter text, textbox 2" & vbNewLine
+            TextBox2.Select()
+
+        End If
+    End Sub
+
+End Class
