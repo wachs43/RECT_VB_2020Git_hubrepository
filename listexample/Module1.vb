ea7bbace28e1e2f0020c701434bf4ef6bc30d917
Alex Wachs <alexwachs310@hotmail.com>
2020-02-20 08:57:36 -0700

    dalb,

---
+++ listexample/Module1.vb
@@ -0,0 +1,22 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim name As New List(Of String)
+
+        name.Add("mac daddy alex")
+        name.Add("big hoss timmothy")
+        name.Add(Console.ReadLine())
+
+        name.Contains(Console.ReadLine())
+
+        For Each n In name
+            Console.WriteLine(name)
+        Next
+
+        Console.ReadLine()
+
+
+    End Sub
+
+End Module
