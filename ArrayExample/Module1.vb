ae89a5b63a29c8b2722b0721204f1ad1461aa44f
Alex Wachs <alexwachs310@hotmail.com>
2020-03-03 09:14:01 -0700

    dskv

---
+++ ArrayExample/Module1.vb
@@ -0,0 +1,36 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim myarray(3) As Integer
+
+        myarray(1) = 5
+
+
+        myarray = {1, 2, 3, 4}
+
+        myarray(0) = 5
+
+        Console.ReadLine()
+
+        ReDim Preserve myarray(15)
+
+        Dim infoarray() As String
+
+        infoarray = Split("hello,goodbye,pizza,hungry,ice cream", ",")
+
+        For Each thingy In infoarray
+
+            Console.WriteLine(thingy)
+
+            infoarray.Contains(thingy)
+
+        Next
+
+        For i = LBound(infoarray) To UBound(infoarray)
+            Console.WriteLine("at" & Str(i) & "value is:" & infoarray(i))
+        Next
+
+    End Sub
+
+End Module
