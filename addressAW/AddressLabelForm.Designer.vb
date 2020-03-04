9de5fe1ff88a07da4b3e191863e572d7ef473910
Alex Wachs <alexwachs310@hotmail.com>
2020-03-04 15:55:00 -0700

    fixed dispaly

--- addressAW/AddressLabelForm.Designer.vb
+++ addressAW/AddressLabelForm.Designer.vb
@@ -36,10 +36,12 @@ Partial Class AddressLabelForm
         Me.StreetAddressBox = New System.Windows.Forms.TextBox()
         Me.FirstNameBox = New System.Windows.Forms.TextBox()
         Me.AddressLabel = New System.Windows.Forms.GroupBox()
+        Me.Label7 = New System.Windows.Forms.Label()
         Me.DisplayLabelButton = New System.Windows.Forms.Button()
         Me.ClearButton = New System.Windows.Forms.Button()
         Me.ExitButton = New System.Windows.Forms.Button()
         Me.MailingAddressBox.SuspendLayout()
+        Me.AddressLabel.SuspendLayout()
         Me.SuspendLayout()
         '
         'MailingAddressBox
@@ -161,12 +163,21 @@ Partial Class AddressLabelForm
         '
         'AddressLabel
         '
+        Me.AddressLabel.Controls.Add(Me.Label7)
         Me.AddressLabel.Location = New System.Drawing.Point(393, 12)
         Me.AddressLabel.Name = "AddressLabel"
         Me.AddressLabel.Size = New System.Drawing.Size(353, 239)
         Me.AddressLabel.TabIndex = 1
         Me.AddressLabel.TabStop = False
-        Me.AddressLabel.Text = "Address Label"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(20, 0)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(74, 13)
+        Me.Label7.TabIndex = 0
+        Me.Label7.Text = "Address Label"
         '
         'DisplayLabelButton
         '
@@ -195,7 +206,7 @@ Partial Class AddressLabelForm
         Me.ExitButton.Text = "Exit"
         Me.ExitButton.UseVisualStyleBackColor = True
         '
-        'Form1
+        'AddressLabelForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
@@ -205,10 +216,12 @@ Partial Class AddressLabelForm
         Me.Controls.Add(Me.DisplayLabelButton)
         Me.Controls.Add(Me.AddressLabel)
         Me.Controls.Add(Me.MailingAddressBox)
-        Me.Name = "Form1"
+        Me.Name = "AddressLabelForm"
         Me.Text = "Form1"
         Me.MailingAddressBox.ResumeLayout(False)
         Me.MailingAddressBox.PerformLayout()
+        Me.AddressLabel.ResumeLayout(False)
+        Me.AddressLabel.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
@@ -230,4 +243,5 @@ Partial Class AddressLabelForm
     Friend WithEvents Label1 As Label
     Friend WithEvents ZIPBox As TextBox
     Friend WithEvents StateBox As TextBox
+    Friend WithEvents Label7 As Label
 End Class
--- addressAW/AddressLabelForm.vb
+++ addressAW/AddressLabelForm.vb
@@ -6,13 +6,13 @@
 
 Public Class AddressLabelForm
     Private Sub DisplayButton_Click(sender As Object, e As EventArgs) Handles DisplayLabelButton.Click
-        AddressLabel.Text &= FirstNameBox.Text & " " & LastNameBox.Text & vbNewLine & StreetAddressBox.Text & vbNewLine & CityBox.Text & ", " & StateBox.Text & " " & ZIPBox.Text
+        AddressLabel.Text &= vbNewLine & FirstNameBox.Text & " " & LastNameBox.Text & vbNewLine & StreetAddressBox.Text & vbNewLine & CityBox.Text & ", " & StateBox.Text & " " & ZIPBox.Text
     End Sub
     Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
         AddressLabel.Text = ""
     End Sub
-        Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
-            Me.Close()
-        End Sub
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
 
-    End Class
+End Class
